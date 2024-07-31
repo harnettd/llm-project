7199a64b27e1dcc61c9a9e4e91e20f514111602a
Derek Harnett <derek.harnett@shaw.ca>
2024-07-31 12:02:17 -0700

    use reqparse for args in app.api

--- app/api.py
+++ app/api.py
@@ -1,13 +1,12 @@
 """Set up a (local) server that takes in POST requests consisting of a 
-list of (text) movie reviews and returns a correpsonding classification
+list of (text) movie reviews and returns a corresponding classification
 of those reviews: a 1 (thumbs-up) for a positive review and a 0 (thumbs-down)
-for a negative review. (Note that each movie review's classification is 
-actually returned as a pair of probabilities.)   
+for a negative review.   
 """
 import pickle
 
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 from pathlib import Path
 
 from app.cleaner.preprocessor import Preprocessor
@@ -18,19 +17,23 @@ api = Api(app)
 
 # Load the fitted classifier.
 project_path = Path(__file__).parent.parent
-model_path = project_path / 'models' / 'best_model.pkl'
+model_path = project_path / 'app' / 'model' / 'best_model.pkl'
 with open(model_path, 'rb') as file:
     classifier = pickle.load(file) 
 
-class ClassifyMovieReview(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument('reviews', type=list, location='json', required=True,
+                    help='List of movie reviews cannot be parsed')
+
+class ClassifyMovieReviews(Resource):
     """Respond to POST requests of lists of movie reviews."""
     def post(self):
-        json_data = request.get_json()
-        reviews = json_data['text']
-        return classifier.predict(reviews) 
+        args = parser.parse_args()
+        reviews = args['reviews']
+        return {'labels': classifier.predict(reviews).tolist()}, 200
 
-api.add_resource(ClassifyMovieReview, '/classify')
+api.add_resource(ClassifyMovieReviews, '/classify')
     
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
